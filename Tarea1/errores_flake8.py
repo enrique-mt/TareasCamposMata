44bf161a8f5d82cfc4ca4308934d42dc1ee77166
enrique-mt <200436756+enrique-mt@users.noreply.github.com>
2025-02-27 14:25:12 -0600

    Añade el código el código de python con tres errores de flake8

---
+++ Tarea1/errores_flake8.py
@@ -0,0 +1,4 @@
+def hola(x):
+    string="Hola," + x  # Faltan espacios alredore del operador
+    return string  # Faltan dos líneas en blanco luego de terminar la función
+print(hola('Pedro'))# Faltan dos espacios antes del comentario sobre misa línea
